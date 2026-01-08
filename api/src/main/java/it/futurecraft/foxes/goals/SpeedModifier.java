795071e5ebe18146eb60e16f81b076833067b444
danieleguglietti <gugliettidaniele@gmail.com>
2026-01-08 19:12:17 +0100

    first goals

---
+++ api/src/main/java/it/futurecraft/foxes/goals/SpeedModifier.java
@@ -0,0 +1,12 @@
+package it.futurecraft.foxes.goals;
+
+/**
+ * A goal which modifies the speed of the tamable.
+ */
+public interface SpeedModifier {
+    /**
+     * Returns the speed modifier of this goal.
+     * @return The speed modifier value.
+     */
+    double speedModifier();
+}
