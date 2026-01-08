8753c61485bb65ebece9e84d40f9a83e2d3f64a0
danieleguglietti <gugliettidaniele@gmail.com>
2026-01-08 02:33:06 +0100

    custom fox

---
+++ api/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+   id("foxes.common-conventions")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+}
---
+++ paper/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("foxes.build-conventions")
+    id("foxes.common-conventions")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+
+    implementation(project(":api"))
+}
